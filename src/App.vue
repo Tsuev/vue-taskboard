7cf6f83529b9aff54fb9f67333e7b7ac3d8b0900
Turpal <bertaevwf@gmail.com>
2021-07-17 16:30:21 +0300

    Редизайн сайта

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,37 @@ export default {
 </script>
 
 <style>
-
+  /* .btn-primary {
+    animation: btn-glitch 1s infinite;
+    transition: 1s ease-in !important;
+  }
+  @keyframes btn-glitch {
+    0% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 0%, rgba(211,80,165,1) 11%, rgba(76,56,133,1) 19%);
+    }
+    10% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 13%, rgba(211,80,165,1) 37%, rgba(76,56,133,1) 58%);
+    }
+    25% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 18%, rgba(211,80,165,1) 50%, rgba(76,56,133,1) 77%);
+    }
+    40% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 69%, rgba(211,80,165,1) 94%, rgba(76,56,133,1) 100%);
+    }
+    50% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 40%, rgba(211,80,165,1) 66%, rgba(76,56,133,1) 87%);       
+    }
+    60% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 69%, rgba(211,80,165,1) 94%, rgba(76,56,133,1) 100%);
+    }
+    75% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 18%, rgba(211,80,165,1) 50%, rgba(76,56,133,1) 77%);
+    }
+    90% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 13%, rgba(211,80,165,1) 37%, rgba(76,56,133,1) 58%);
+    }
+    100% {
+      background: linear-gradient(90deg, rgba(189,63,151,1) 0%, rgba(211,80,165,1) 11%, rgba(76,56,133,1) 19%);
+    }
+  } */
 </style>
---
+++ src/components/Logo/Logo.vue
@@ -0,0 +1,271 @@
+<template>
+    <a @click='$router.push({name: "home"})' class="aside-logo"  >
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.2" baseProfile="tiny-ps" viewBox="0 0 530 814" width="530" height="814">
+        <defs>
+            <radialGradient id="grd1" gradientUnits="userSpaceOnUse" cx="256" cy="458" r="726.364">
+                <stop offset="0" stop-color="#b948a4"/>
+                <stop offset="0.999" stop-color="#181e25" stop-opacity="0"/>
+            </radialGradient>
+            <linearGradient id="grd2" gradientUnits="userSpaceOnUse" x1="199.235" y1="9.683" x2="514.693" y2="9.683">
+                <stop offset="0" stop-color="#6b4ea8"/>
+                <stop offset="1" stop-color="#211a53"/>
+            </linearGradient>
+            <linearGradient id="grd3" gradientUnits="userSpaceOnUse" x1="281.466" y1="-12.938" x2="552.117" y2="149.114">
+                <stop offset="0" stop-color="#6b4ea8" stop-opacity="0"/>
+                <stop offset="1" stop-color="#211a53"/>
+            </linearGradient>
+            <linearGradient id="grd4" gradientUnits="userSpaceOnUse" x1="287.571" y1="576.811" x2="287.571" y2="57.033">
+                <stop offset="0" stop-color="#835daf"/>
+                <stop offset="1" stop-color="#8c54ab"/>
+            </linearGradient>
+            <linearGradient id="grd5" gradientUnits="userSpaceOnUse" x1="460.784" y1="84.895" x2="671.228" y2="153.037">
+                <stop offset="0" stop-color="#8f60b0"/>
+                <stop offset="1" stop-color="#8c54ab" stop-opacity="0"/>
+            </linearGradient>
+            <linearGradient id="grd6" gradientUnits="userSpaceOnUse" x1="317.496" y1="189.495" x2="595.253" y2="326.136">
+                <stop offset="0" stop-color="#6b4ea8"/>
+                <stop offset="1" stop-color="#211a53" stop-opacity="0"/>
+            </linearGradient>
+            <linearGradient id="grd7" gradientUnits="userSpaceOnUse" x1="17.307" y1="228.038" x2="332.765" y2="228.038">
+                <stop offset="0" stop-color="#371b55"/>
+                <stop offset="1" stop-color="#fb44a1"/>
+            </linearGradient>
+            <linearGradient id="grd8" gradientUnits="userSpaceOnUse" x1="318.308" y1="334.408" x2="503.925" y2="589.476">
+                <stop offset="0" stop-color="#371b55"/>
+                <stop offset="1" stop-color="#ea57aa"/>
+            </linearGradient>
+            <linearGradient id="grd9" gradientUnits="userSpaceOnUse" x1="8.748" y1="753.816" x2="8.748" y2="234.038">
+                <stop offset="0" stop-color="#c05dae"/>
+                <stop offset="1" stop-color="#e466b1"/>
+            </linearGradient>
+            <linearGradient id="grd10" gradientUnits="userSpaceOnUse" x1="62.743" y1="147.03" x2="441.997" y2="320.702">
+                <stop offset="0" stop-color="#bc2c91"/>
+                <stop offset="1" stop-color="#e466b1" stop-opacity="0"/>
+            </linearGradient>
+            <linearGradient id="grd11" gradientUnits="userSpaceOnUse" x1="214.504" y1="621.354" x2="-63.253" y2="484.713">
+                <stop offset="0" stop-color="#6b4ea8"/>
+                <stop offset="1" stop-color="#211a53" stop-opacity="0"/>
+            </linearGradient>
+        </defs>
+            
+            
+            <g>
+                <path class="shp0" d="M256 -270.81L256 -270.81C-146.51 -270.81 -472.81 55.49 -472.81 458L-472.81 458C-472.81 860.51 -146.51 1186.81 256 1186.81L256 1186.81C658.51 1186.81 984.81 860.51 984.81 458L984.81 458C984.81 55.49 658.51 -270.81 256 -270.81Z"/>
+            </g>
+            <g>
+                <g>
+                    <g>
+                        <path class="shp1" d="M290.89 15.68L199.24 15.68L387.77 384.27L293.5 576.81L404.49 576.81C421.08 576.81 436.23 567.36 443.53 552.46L509.96 416.78C519.97 396.32 519.76 372.33 509.39 352.05L358.49 57.03C345.51 31.65 319.4 15.68 290.89 15.68Z"/>
+                        <path class="shp2" d="M290.89 15.68L199.24 15.68L387.77 384.27L293.5 576.81L404.49 576.81C421.08 576.81 436.23 567.36 443.53 552.46L509.96 416.78C519.97 396.32 519.76 372.33 509.39 352.05L358.49 57.03C345.51 31.65 319.4 15.68 290.89 15.68Z"/>
+                        <path class="shp3" d="M358.49 57.03C433.64 203.97 439.51 278.59 387.77 384.27L293.5 576.81L404.49 576.81C421.08 576.81 436.23 567.36 443.53 552.46L509.96 416.78C519.97 396.32 519.76 372.33 509.39 352.05L358.49 57.03Z"/>
+                        <path class="shp4" d="M358.49 57.03C433.64 203.97 439.51 278.59 387.77 384.27L293.5 576.81L404.49 576.81C421.08 576.81 436.23 567.36 443.53 552.46L509.96 416.78C519.97 396.32 519.76 372.33 509.39 352.05L358.49 57.03Z"/>
+                        <path class="shp5" d="M509.39 352.05L466.81 268.82C469.72 316.5 458.2 359.98 433.43 410.56L352.04 576.81L404.49 576.81C421.08 576.81 436.23 567.36 443.53 552.46L509.96 416.78C519.97 396.32 519.76 372.33 509.39 352.05Z"/>
+                    </g>
+                    <g>
+                        <path class="shp6" d="M241.11 795.17L332.76 795.17L144.23 426.58L238.5 234.04L127.51 234.04C110.91 234.04 95.77 243.49 88.47 258.39L22.04 394.07C12.03 414.53 12.24 438.52 22.61 458.8L173.51 753.82C186.49 779.2 212.6 795.17 241.11 795.17Z"/>
+                        <path class="shp7" d="M241.11 795.17L332.76 795.17L144.23 426.58L238.5 234.04L127.51 234.04C110.91 234.04 95.77 243.49 88.47 258.39L22.04 394.07C12.03 414.53 12.24 438.52 22.61 458.8L173.51 753.82C186.49 779.2 212.6 795.17 241.11 795.17Z"/>
+                        <path class="shp8" d="M173.51 753.82C98.36 606.88 92.49 532.26 144.23 426.58L238.5 234.04L127.51 234.04C110.91 234.04 95.77 243.49 88.47 258.39L22.04 394.07C12.03 414.53 12.24 438.52 22.61 458.8L173.51 753.82Z"/>
+                        <path class="shp9" d="M173.51 753.82C98.36 606.88 92.49 532.26 144.23 426.58L238.5 234.04L127.51 234.04C110.91 234.04 95.77 243.49 88.47 258.39L22.04 394.07C12.03 414.53 12.24 438.52 22.61 458.8L173.51 753.82Z"/>
+                        <path class="shp10" d="M22.61 458.8L65.19 542.03C62.28 494.35 73.8 450.87 98.57 400.28L179.96 234.04L127.51 234.04C110.91 234.04 95.77 243.49 88.47 258.39L22.04 394.07C12.03 414.53 12.24 438.52 22.61 458.8Z"/>
+                    </g>
+                </g>
+            </g>
+        </svg>
+        <div class="logo-title">TMQ</div>
+    </a>
+</template>
+<style scoped>
+    .aside-logo {
+        padding: 5px  10px;
+        display: flex;
+        align-content: center;
+        align-items: center;
+        justify-content: center;
+        
+    }
+    .aside-logo  {
+ 
+        text-decoration: none;
+        -webkit-animation-duration:2s;
+        animation-duration:2s;
+        -webkit-animation-timing-function:linear;
+        animation-timing-function:linear;
+        -webkit-animation-name:glitch-anim;
+        animation-name:glitch-anim;
+        -webkit-animation-direction:alternate;
+        animation-direction:alternate
+    }
+    svg {
+        max-width: 50px;
+        max-height: 38px;
+    }
+    .sidebar-brand {
+        display: block;
+        height: 54px;
+        width: 100%;
+        padding: 0;
+    }
+    .logo-title {
+        font-family: 'Cyberpunk';
+        font-size: 2rem;
+        background: linear-gradient(90deg, rgba(189,63,151,1) 0%, rgba(211,80,165,1) 35%, rgba(76,56,133,1) 66%);
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+    @font-face {
+        font-family: 'Cyberpunk';
+        src: url('../../assets/fonts/Cyberpunk-Regular.woff2') format('woff2'),
+            url('../../assets/fonts/Cyberpunk-Regular.woff') format('woff');
+        font-weight: normal;
+        font-style: normal;
+        font-display: swap;
+    }
+
+
+    tspan { white-space:pre }
+    .shp0 { opacity: 0.22;fill: url(#grd1) } 
+    .shp1 { fill: url(#grd2) } 
+    .shp2 { fill: url(#grd3) } 
+    .shp3 { fill: url(#grd4) } 
+    .shp4 { fill: url(#grd5) } 
+    .shp5 { opacity: 0.42;fill: url(#grd6) } 
+    .shp6 { fill: url(#grd7) } 
+    .shp7 { fill: url(#grd8) } 
+    .shp8 { fill: url(#grd9) } 
+    .shp9 { fill: url(#grd10) } 
+    .shp10 { opacity: 0.322;fill: url(#grd11) } 
+ 
+    @keyframes glitch-anim {
+    0% {
+    opacity:1;
+    -webkit-transform:translateZ(0);
+    transform:translateZ(0);
+    -webkit-clip-path:polygon(0 2%,100% 2%,100% 5%,0 5%);
+    clip-path:polygon(0 2%,100% 2%,100% 5%,0 5%)
+    }
+    2% {
+    -webkit-clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    -webkit-transform:translate(-5px);
+    transform:translate(-5px)
+    }
+    6% {
+    -webkit-clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    -webkit-transform:translate(5px);
+    transform:translate(5px)
+    }
+    8% {
+    -webkit-clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    -webkit-transform:translate(-5px);
+    transform:translate(-5px)
+    }
+    9% {
+    -webkit-clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    clip-path:polygon(0 78%,100% 78%,100% 100%,0 100%);
+    -webkit-transform:translate(0);
+    transform:translate(0)
+    }
+    10% {
+    -webkit-clip-path:polygon(0 54%,100% 54%,100% 44%,0 44%);
+    clip-path:polygon(0 54%,100% 54%,100% 44%,0 44%);
+    -webkit-transform:translate3d(5px,0,0);
+    transform:translate3d(5px,0,0)
+    }
+    13% {
+    -webkit-clip-path:polygon(0 54%,100% 54%,100% 44%,0 44%);
+    clip-path:polygon(0 54%,100% 54%,100% 44%,0 44%);
+    -webkit-transform:translateZ(0);
+    transform:translateZ(0)
+    }
+    13.1% {
+    -webkit-clip-path:polygon(0 0,0 0,0 0,0 0);
+    clip-path:polygon(0 0,0 0,0 0,0 0);
+    -webkit-transform:translate3d(5px,0,0);
+    transform:translate3d(5px,0,0)
+    }
+    15% {
+    -webkit-clip-path:polygon(0 60%,100% 60%,100% 40%,0 40%);
+    clip-path:polygon(0 60%,100% 60%,100% 40%,0 40%);
+    -webkit-transform:translate3d(5px,0,0);
+    transform:translate3d(5px,0,0)
+    }
+    20% {
+    -webkit-clip-path:polygon(0 60%,100% 60%,100% 40%,0 40%);
+    clip-path:polygon(0 60%,100% 60%,100% 40%,0 40%);
+    -webkit-transform:translate3d(-5px,0,0);
+    transform:translate3d(-5px,0,0)
+    }
+    20.1% {
+    -webkit-clip-path:polygon(0 0,0 0,0 0,0 0);
+    clip-path:polygon(0 0,0 0,0 0,0 0);
+    -webkit-transform:translate3d(5px,0,0);
+    transform:translate3d(5px,0,0)
+    }
+    25% {
+    -webkit-clip-path:polygon(0 85%,100% 85%,100% 40%,0 40%);
+    clip-path:polygon(0 85%,100% 85%,100% 40%,0 40%);
+    -webkit-transform:translate3d(5px,0,0);
+    transform:translate3d(5px,0,0)
+    }
+    30% {
+    -webkit-clip-path:polygon(0 85%,100% 85%,100% 40%,0 40%);
+    clip-path:polygon(0 85%,100% 85%,100% 40%,0 40%);
+    -webkit-transform:translate3d(-5px,0,0);
+    transform:translate3d(-5px,0,0)
+    }
+    30.1% {
+    -webkit-clip-path:polygon(0 0,0 0,0 0,0 0);
+    clip-path:polygon(0 0,0 0,0 0,0 0)
+    }
+    35% {
+    -webkit-clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    -webkit-transform:translate(-5px);
+    transform:translate(-5px)
+    }
+    40% {
+    -webkit-clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    -webkit-transform:translate(5px);
+    transform:translate(5px)
+    }
+    45% {
+    -webkit-clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    -webkit-transform:translate(-5px);
+    transform:translate(-5px)
+    }
+    50% {
+    -webkit-clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    clip-path:polygon(0 63%,100% 63%,100% 80%,0 80%);
+    -webkit-transform:translate(0);
+    transform:translate(0)
+    }
+    55% {
+    -webkit-clip-path:polygon(0 10%,100% 10%,100% 0,0 0);
+    clip-path:polygon(0 10%,100% 10%,100% 0,0 0);
+    -webkit-transform:translate3d(5px,0,0);
+    transform:translate3d(5px,0,0)
+    }
+    60% {
+    -webkit-clip-path:polygon(0 10%,100% 10%,100% 0,0 0);
+    clip-path:polygon(0 10%,100% 10%,100% 0,0 0);
+    -webkit-transform:translateZ(0);
+    transform:translateZ(0);
+    opacity:1
+    }
+    60.1% {
+    -webkit-clip-path:polygon(0 0,0 0,0 0,0 0);
+    clip-path:polygon(0 0,0 0,0 0,0 0);
+    opacity:1
+    }
+    to {
+    -webkit-clip-path:polygon(0 0,0 0,0 0,0 0);
+    clip-path:polygon(0 0,0 0,0 0,0 0);
+    opacity:1
+        }
+    }
+</style>
+ 
